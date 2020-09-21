6f4bca24c70f410f8389a362281618c21725bf13
habimanasharif <hdidiersharif@gmail.com>
2020-09-21 22:56:34 +0200

    Updates

---
+++ precache-manifest.d50ede57b05f4f08ac6c320143fb9004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cdc904d443ef5e5093998282303240",
+    "url": "/habimanasharif/ExP-Traker/index.html"
+  },
+  {
+    "revision": "204742d441451873cc98",
+    "url": "/habimanasharif/ExP-Traker/static/css/main.4090f3f7.chunk.css"
+  },
+  {
+    "revision": "28c205204b2a66e60732",
+    "url": "/habimanasharif/ExP-Traker/static/js/2.1b74b62e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/habimanasharif/ExP-Traker/static/js/2.1b74b62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204742d441451873cc98",
+    "url": "/habimanasharif/ExP-Traker/static/js/main.22dea64c.chunk.js"
+  },
+  {
+    "revision": "af2fa97b4331cbb70e24",
+    "url": "/habimanasharif/ExP-Traker/static/js/runtime-main.3e740f39.js"
+  }
+]);
